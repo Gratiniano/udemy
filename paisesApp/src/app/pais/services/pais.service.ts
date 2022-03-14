4aa0949fd871257d3c6e7a7b630ee4efbc460f4a
gratiniano <gratiniano@gmail.com>
2022-03-14 06:48:22 +0100

    Leccion 107

--- paisesApp/src/app/pais/services/pais.service.ts
+++ paisesApp/src/app/pais/services/pais.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaisService {
 
-  constructor() { }
+  private apiUrl:string = 'https://restcountries.com/v2/';
+
+  constructor(private http: HttpClient) {}
+
+  buscarPais(termino:string): Observable<any> {
+    const url = `${ this.apiUrl }/name/${ termino }`;
+    return this.http.get(url);
+  }
 }
