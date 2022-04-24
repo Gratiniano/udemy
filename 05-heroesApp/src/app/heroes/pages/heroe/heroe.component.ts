f4e8fcaffb58eb0ff0f79120142d8ffc87d873e2
gratiniano <gratiniano@gmail.com>
2022-04-24 21:10:00 +0200

    205. SECCION 14. CRUD

--- 05-heroesApp/src/app/heroes/pages/heroe/heroe.component.ts
+++ 05-heroesApp/src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { switchMap } from 'rxjs';
 import { Heroe } from '../../interfaces/heroes.interface';
 import { HeroesService } from '../../services/heroes.service';
@@ -8,6 +8,12 @@ import { HeroesService } from '../../services/heroes.service';
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
   styles: [
+    `
+      img {
+        width:100%;
+        border-radius:5px;
+      }
+    `
   ]
 })
 export class HeroeComponent implements OnInit {
@@ -15,7 +21,8 @@ export class HeroeComponent implements OnInit {
   heroe!: Heroe;
 
   constructor(private activatedRoute: ActivatedRoute,
-              private heroesService: HeroesService) { }
+              private heroesService: HeroesService,
+              private router: Router) { }
 
   ngOnInit(): void {
     this.activatedRoute.params
@@ -23,6 +30,10 @@ export class HeroeComponent implements OnInit {
         switchMap( ({ id }) => this.heroesService.getHeroePorId(id) )
       )
       .subscribe( heroe => this.heroe = heroe);
+  }
+
+  regresar(){
+    this.router.navigate(['/heroes/listado']);
   }
 
 }
